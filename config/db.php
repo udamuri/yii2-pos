d1893f2f13431a56d0d57eb855bc51995604f734
Muri Budiman <udamuri@gmail.com>
2017-02-17 16:23:35 +0700

    linux localhost

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=2017_basic_pos',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=2017_basic_pos',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
