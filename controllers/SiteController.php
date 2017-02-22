0c05cbb34bf9f7cd5df1dd016bddf3948ca2bc46
muri.budiman <muri.budiman@redawning.com>
2017-02-22 13:33:43 +0700

    login partial render

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -84,7 +84,7 @@ class SiteController extends Controller
         if ($model->load(Yii::$app->request->post()) && $model->login()) {
             return $this->goBack();
         }
-        return $this->render('login', [
+        return $this->renderPartial('login', [
             'model' => $model,
         ]);
     }
--- views/site/login.php
+++ views/site/login.php
@@ -1,33 +1,68 @@
 <?php
 
-/* @var $this yii\web\View */
-/* @var $form yii\bootstrap\ActiveForm */
-/* @var $model app\models\LoginForm */
+/* @var $this \yii\web\View */
+/* @var $content string */
 
 use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use app\assets\AppAsset;
+use app\widgets\Myalert;
 use yii\bootstrap\ActiveForm;
 
-$this->title = 'Login';
-$this->params['breadcrumbs'][] = $this->title;
+AppAsset::register($this);
 ?>
-<div class="row">
-    <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-        <!-- <p>Please fill out the following fields to login:</p> -->
-        <?php $model->rememberMe = 0;?>
-        <?php $form = ActiveForm::begin([
-            'id' => 'login-form',
-        ]); ?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title>LOGIN</title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
 
-            <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+<div class="wrap">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12"></div>
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                <!-- <p>Please fill out the following fields to login:</p> -->
+                <?php $model->rememberMe = 0;?>
+                <?php $form = ActiveForm::begin([
+                    'id' => 'login-form',
+                ]); ?>
 
-            <?= $form->field($model, 'password')->passwordInput() ?>
+                    <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
 
-            <?= $form->field($model, 'rememberMe')->checkbox(['checked'=>false]) ?>
+                    <?= $form->field($model, 'password')->passwordInput() ?>
 
-            <div class="form-group">
-                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                    <?= $form->field($model, 'rememberMe')->checkbox(['checked'=>false]) ?>
+
+                    <div class="form-group">
+                        <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                    </div>
+
+                <?php ActiveForm::end(); ?>
             </div>
-
-        <?php ActiveForm::end(); ?>
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12"></div>
+        </div>
     </div>
 </div>
+
+<footer class="footer">
+    <div class="container-fluid">
+        <p class="pull-left">&copy; Muri <?= date('Y') ?></p>
+
+        <!-- <p class="pull-right">Muri Budiman</p> -->
+    </div>
+</footer>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
